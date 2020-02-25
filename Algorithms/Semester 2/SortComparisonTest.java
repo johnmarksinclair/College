1351d92fed9b0b615b75a86c119acc24c4ea0cf8
johnmarksinclair <sinclairjohn72@gmail.com>
2020-02-25 15:30:22 +0000

    edited test cases

--- Algorithms/Semester 2/SortComparisonTest.java
+++ Algorithms/Semester 2/SortComparisonTest.java
@@ -16,31 +16,39 @@ import static org.junit.Assert.*;
  */
 
 public class SortComparisonTest {
-	public double unsorted[] = { 4, -2, -1, 2, -5, 0, 3, 1, -3, -4, 5 };
-	public double sorted[] = { -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5 };
 
 	@Test
 	public void testInsertionSort() {
+		double unsorted[] = { 4, -2, -1, 2, -5, 0, 3, 1, -3, -4, 5 };
+		double sorted[] = { -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5 };
 		assertTrue(Arrays.equals(sorted, SortComparison.insertionSort(unsorted)));
 	}
 
 	@Test
 	public void testSelectionSort() {
+		double unsorted[] = { 4, -2, -1, 2, -5, 0, 3, 1, -3, -4, 5 };
+		double sorted[] = { -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5 };
 		assertTrue(Arrays.equals(sorted, SortComparison.selectionSort(unsorted)));
 	}
 
 	@Test
 	public void testQuickSort() {
+		double unsorted[] = { 4, -2, -1, 2, -5, 0, 3, 1, -3, -4, 5 };
+		double sorted[] = { -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5 };
 		assertTrue(Arrays.equals(sorted, SortComparison.quickSort(unsorted)));
 	}
 
 	@Test
 	public void testMergeSortRecurssive() {
+		double unsorted[] = { 4, -2, -1, 2, -5, 0, 3, 1, -3, -4, 5 };
+		double sorted[] = { -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5 };
 		assertTrue(Arrays.equals(sorted, SortComparison.mergeSortRecursive(unsorted)));
 	}
 
 	@Test
 	public void testMergeSortIterative() {
+		double unsorted[] = { 4, -2, -1, 2, -5, 0, 3, 1, -3, -4, 5 };
+		double sorted[] = { -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5 };
 		assertTrue(Arrays.equals(sorted, SortComparison.mergeSortIterative(unsorted)));
 	}
 
