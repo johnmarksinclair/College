f15be480ab72b8c231f6f0778a0c26337b2b3aa6
John Sinclair <johnmarksinclair@Johns-MacBook-Pro.local>
2020-10-20 09:12:14 +0100

    fucntional done

--- 3rd Year/Functional Programming/Exercise01/src/Ex01.hs
+++ 3rd Year/Functional Programming/Exercise01/src/Ex01.hs
@@ -4,7 +4,7 @@ import Data.Char (toUpper)
 name, idno, username :: String
 name      =  "Sinclair, John"  -- replace with your name
 idno      =  "16325734"    -- replace with your student id
-username  =  "sinclajo@tcd.ie"   -- replace with your TCD username
+username  =  "sinclajo"   -- replace with your TCD username
 
 
 declaration -- do not modify this
