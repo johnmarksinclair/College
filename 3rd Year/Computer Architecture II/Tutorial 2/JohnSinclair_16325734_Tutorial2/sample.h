bd85d233af8dd18165186cc7d354a4089cf5edb7
johnmarksinclair <45259974+johnmarksinclair@users.noreply.github.com>
2020-11-08 15:10:33 +0000

    ...

--- 3rd Year/Computer Architecture II/Tutorial 2/JohnSinclair_16325734_Tutorial2/sample.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-// Function declaration
-
-// A procedure to calculate sum of the array
-extern "C" _int64 array_proc(_int64, _int64 array[]);
-
-// A procedure to print the sum of two integers
-extern "C" _int64 print_proc(_int64, _int64);
