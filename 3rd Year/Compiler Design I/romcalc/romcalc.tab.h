3208421e5ae18d2224d1ddd7a15eac2ec7131727
John Sinclair <sinclairjohn72@gmail.com>
2021-04-22 14:01:34 +0100

    romancalc

---
+++ 3rd Year/Compiler Design I/romcalc/romcalc.tab.h
@@ -0,0 +1,87 @@
+/* A Bison parser, made by GNU Bison 2.3.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     ADD = 258,
+     SUB = 259,
+     DIV = 260,
+     MUL = 261,
+     OP = 262,
+     CP = 263,
+     M = 264,
+     D = 265,
+     C = 266,
+     L = 267,
+     X = 268,
+     V = 269,
+     I = 270,
+     Z = 271,
+     EOL = 272
+   };
+#endif
+/* Tokens.  */
+#define ADD 258
+#define SUB 259
+#define DIV 260
+#define MUL 261
+#define OP 262
+#define CP 263
+#define M 264
+#define D 265
+#define C 266
+#define L 267
+#define X 268
+#define V 269
+#define I 270
+#define Z 271
+#define EOL 272
+
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+# define YYSTYPE_IS_TRIVIAL 1
+#endif
+
+extern YYSTYPE yylval;
+
