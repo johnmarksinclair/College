2f18768c76af362feffa2db2ea358eafbf2f3989
John Sinclair <sinclairjohn72@gmail.com>
2021-10-27 18:44:46 +0100

    intapps nearly done and ling ready to edit

---
+++ 4th Year/Comp Linguistics/EM Assignment/echo.cpp
@@ -0,0 +1,13 @@
+// make echo
+// ./echo
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "Enter a number" << endl;
+
+    // User can input an integer
+    cin >> a;
+    cout << "User entered number " << a << endl;
+}
